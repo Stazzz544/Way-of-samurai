412de9fce0e162f340e68ab6e183ca1f69c6ed9f
Stas <stazzzdzr@gmail.com>
2021-07-12 20:42:29 +0300

    navigation is ready

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import s from './App.module.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components//Dialogs/Dialogs';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+
 import {
 	BrowserRouter as Router,
 	Switch,
@@ -18,11 +22,19 @@ const App = () => {
 		
 		<div className={s.appWrapper}>
 			<Header/>
+			
+			<Router>
 			<Navbar/>
-			<div className={s.appContentWrapper}>
-				<Route component={<Dialogs/>}></Route>
-				<Route component={<Profile/>}></Route>
-			</div>
+				<Switch>
+					<div className={s.appContentWrapper}>
+						<Route path="/dialogs" component={Dialogs}></Route>
+						<Route path="/profile" component={Profile}></Route>
+						<Route path="/news" component={News}></Route>
+						<Route path="/Music" component={Music}></Route>
+						<Route path="/settings" component={Settings}></Route>
+					</div>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,28 @@
 import s from './Navbar.module.css';
+import {
+	BrowserRouter as Router,
+	NavLink 
+} from "react-router-dom";
+
+
 
 const Navbar = () => {
-	return(
+	return (
 		<nav className={s.nav}>
 			<div className={s.item}>
-				<a className={s.link} href="/Profile">Profile</a>
+				<NavLink activeClassName={s.active} className={s.link} to='/profile'>Profile</NavLink>
 			</div>
 			<div className={s.item}>
-				<a className={s.link} href="Dialogs">Messages</a>
+				<NavLink activeClassName={s.active} className={s.link} to='/dialogs'>Messages</NavLink>
 			</div>
 			<div className={s.item}>
-				<a className={s.link} href="#">News</a>
+				<NavLink activeClassName={s.active} className={s.link} to='/news'>News</NavLink>
 			</div>
 			<div className={s.item}>
-				<a className={s.link} href="#">Music</a>
+				<NavLink activeClassName={s.active} className={s.link} to='/Music'>Music</NavLink>
 			</div>
 			<div className={s.item}>
-				<a className={s.link} href="#">Settings</a>
+				<NavLink activeClassName={s.active} className={s.link} to='/settings'>Settings</NavLink>
 			</div>
 		</nav>
 	)
