f353fff5d735ed8166c5069fad86fb097d321bf1
Stas <stazzzdzr@gmail.com>
2021-08-09 22:40:40 +0300

    redux dev tools

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers, createStore, compose, applyMiddleware} from "redux";
 
 import dialogsReducer from './dialogsReducer';
 import profileReducer from './profileReducer';
@@ -14,7 +14,11 @@ let reducers = combineReducers({
 	usersPage: usersReducer
 })
 
+//для работы redux dev tools использовать код ниже:
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
-let store = createStore(reducers);
+//ниже оригинальный код. Если его раскомментить - нужно закоментить код выше.
+//let store = createStore(reducers);
 
 export default store;
